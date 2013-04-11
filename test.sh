beddd2cf3ee2a462b12be60501192714d4ec87fb
maning <emmanuel.sambale@gmail.com>
2013-04-11 10:22:59 +0800

    add pep8 tests

--- test.sh
+++ test.sh
@@ -7,12 +7,5 @@ pep8 --show-source --ignore=E225,E111,E231,E701 ${files}
 
 echo "===== spellcheck with ispell =====" 
 
-#for f in ${files}; do ispell check $f; done
-
-#ispell list
-#cat *.txt | ispell list | sort -u
-#echo "======  pyflakes  ======"
-#pyflakes $1
-#echo "======  pylint  ======"
-pylint --output-format=parseable ${files}
+for f in ${files}; do ispell check $f; done
 
